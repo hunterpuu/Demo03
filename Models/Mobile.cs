b842908e1229993af4907152fc9e8496a16fa4b8
Sumasg <Sumasg@hotmail.com>
2022-05-06 14:00:01 +0700

    Add project files.

---
+++ Models/Mobile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Demo03.Models
+{
+    public partial class Mobile
+    {
+        public int Id { get; set; }
+        public string Devices { get; set; }
+        public string Os { get; set; }
+        public string Brands { get; set; }
+    }
+}
---
+++ Models/MobileContext.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Demo03.Models
+{
+    public partial class MobileContext : DbContext
+    {
+        public MobileContext()
+        {
+        }
+
+        public MobileContext(DbContextOptions<MobileContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Mobile> Mobiles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=192.168.253.197;Database=Mobile;User Id=sa;Password=password@1;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Thai_CI_AS");
+
+            modelBuilder.Entity<Mobile>(entity =>
+            {
+                entity.ToTable("Mobile");
+
+                entity.Property(e => e.Brands)
+                    .IsRequired()
+                    .HasMaxLength(10)
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.Devices)
+                    .IsRequired()
+                    .HasMaxLength(10)
+                    .IsFixedLength(true);
+
+                entity.Property(e => e.Os)
+                    .IsRequired()
+                    .HasMaxLength(10)
+                    .HasColumnName("OS")
+                    .IsFixedLength(true);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
